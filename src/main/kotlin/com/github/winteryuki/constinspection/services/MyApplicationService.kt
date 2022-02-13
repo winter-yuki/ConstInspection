de31ce02c78048db53392b0abdb5e9379db6b6b1
GitHub Action <action@github.com>
2022-02-13 13:47:10 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/winteryuki/constinspection/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.winteryuki.constinspection.services
+
+import com.github.winteryuki.constinspection.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/winteryuki/constinspection/services/MyProjectService.kt
+++ src/main/kotlin/com/github/winteryuki/constinspection/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.winteryuki.constinspection.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.winteryuki.constinspection.MyBundle
 
 class MyProjectService(project: Project) {
 
